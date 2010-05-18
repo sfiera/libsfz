dc390fcbf2cce01bc08b7c5e6cfbb284942d4966
Chris Pickel <sfiera@gmail.com>
2010-05-18 09:10:52 -0400

    Add sfz/sfz.hpp header which includes everything.

---
+++ include/sfz/sfz.hpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2010 Chris Pickel <sfiera@gmail.com>
+//
+// This file is part of libsfz, a free software project.  You can redistribute it and/or modify it
+// under the terms of the MIT License.
+
+#ifndef SFZ_SFZ_HPP_
+#define SFZ_SFZ_HPP_
+
+#include "sfz/Bytes.hpp"
+#include "sfz/Encoding.hpp"
+#include "sfz/Exception.hpp"
+#include "sfz/Foreach.hpp"
+#include "sfz/Format.hpp"
+#include "sfz/Formatter.hpp"
+#include "sfz/Macros.hpp"
+#include "sfz/MappedFile.hpp"
+#include "sfz/NetworkBytes.hpp"
+#include "sfz/PosixFormatter.hpp"
+#include "sfz/PrintItem.hpp"
+#include "sfz/PrintTarget.hpp"
+#include "sfz/Range.hpp"
+#include "sfz/ReadItem.hpp"
+#include "sfz/ReadSource.hpp"
+#include "sfz/ReferenceCounted.hpp"
+#include "sfz/Rune.hpp"
+#include "sfz/ScopedFd.hpp"
+#include "sfz/Sha1.hpp"
+#include "sfz/SmartPtr.hpp"
+#include "sfz/String.hpp"
+#include "sfz/StringUtilities.hpp"
+#include "sfz/WriteItem.hpp"
+#include "sfz/WriteTarget.hpp"
+
+#endif  // SFZ_SFZ_HPP_
