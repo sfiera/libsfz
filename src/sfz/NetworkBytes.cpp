5a2cc86f6fa00379620e57ade9c1971914668fd1
Chris Pickel <sfiera@gmail.com>
2010-07-01 23:11:17 -0400

    Use {hton,ntoh}{s,l} in NetworkBytes.cpp.
    The OSByteOrder.h functions (well, macros) are non-portable, and on Darwin,
    that's what the arpa/inet.h functions call anyway.  The one hitch is the lack
    of a {hton,ntoh}ll function, which we implement ourselves in terms of the
    others.

--- src/sfz/NetworkBytes.cpp
+++ src/sfz/NetworkBytes.cpp
@@ -5,30 +5,43 @@
 
 #include "sfz/NetworkBytes.hpp"
 
+#include <arpa/inet.h>
 #include <string.h>
-#include <libkern/OSByteOrder.h>
 
 namespace sfz {
 
 namespace {
 
+// There is no standard {hton,ntoh}ll() function, so we implement it here instead.
+inline uint64_t swap_u64(uint64_t u64) {
+    // Test the result of the htons() function.  In the worst case, this is probably a function
+    // call resulting in a couple of bitwise operations, in which case this test should be cheap.
+    // In the best case (on Darwin, for example), htons() is a macro, and an optimizing compiler
+    // can eliminate the test entirely.
+    if (htons(0x0001) == 0x0100) {
+        return ((0ull + htonl(u64 & 0xffffffff)) << 32) | htonl(u64 >> 32);
+    } else {
+        return u64;
+    }
+}
+
 // Convert from network to host byte order.  Overloaded for the integral types int{16,32,64}_t and
 // uint{16,32,64}_t.
-inline int16_t network_to_host(const int16_t& t) { return OSSwapBigToHostInt16(t); }
-inline uint16_t network_to_host(const uint16_t& t) { return OSSwapBigToHostInt16(t); }
-inline int32_t network_to_host(const int32_t& t) { return OSSwapBigToHostInt32(t); }
-inline uint32_t network_to_host(const uint32_t& t) { return OSSwapBigToHostInt32(t); }
-inline int64_t network_to_host(const int64_t& t) { return OSSwapBigToHostInt64(t); }
-inline uint64_t network_to_host(const uint64_t& t) { return OSSwapBigToHostInt64(t); }
+inline int16_t network_to_host(const int16_t& t) { return ntohs(t); }
+inline uint16_t network_to_host(const uint16_t& t) { return ntohs(t); }
+inline int32_t network_to_host(const int32_t& t) { return ntohl(t); }
+inline uint32_t network_to_host(const uint32_t& t) { return ntohl(t); }
+inline int64_t network_to_host(const int64_t& t) { return swap_u64(t); }
+inline uint64_t network_to_host(const uint64_t& t) { return swap_u64(t); }
 
 // Convert from host to network byte order.  Overloaded for the integral types int{16,32,64}_t and
 // uint{16,32,64}_t.
-inline int16_t host_to_network(const int16_t& t) { return OSSwapHostToBigInt16(t); }
-inline uint16_t host_to_network(const uint16_t& t) { return OSSwapHostToBigInt16(t); }
-inline int32_t host_to_network(const int32_t& t) { return OSSwapHostToBigInt32(t); }
-inline uint32_t host_to_network(const uint32_t& t) { return OSSwapHostToBigInt32(t); }
-inline int64_t host_to_network(const int64_t& t) { return OSSwapHostToBigInt64(t); }
-inline uint64_t host_to_network(const uint64_t& t) { return OSSwapHostToBigInt64(t); }
+inline int16_t host_to_network(const int16_t& t) { return htons(t); }
+inline uint16_t host_to_network(const uint16_t& t) { return htons(t); }
+inline int32_t host_to_network(const int32_t& t) { return htonl(t); }
+inline uint32_t host_to_network(const uint32_t& t) { return htonl(t); }
+inline int64_t host_to_network(const int64_t& t) { return swap_u64(t); }
+inline uint64_t host_to_network(const uint64_t& t) { return swap_u64(t); }
 
 }  // namespace
 
@@ -62,7 +75,8 @@ T NetworkBytes<T>::value() const {
 
 template <typename T>
 void NetworkBytes<T>::set_value(T t) {
-    *reinterpret_cast<T*>(_data) = host_to_network(t);
+    const T swapped = host_to_network(t);
+    memcpy(_data, &swapped, size());
 }
 
 // Explicit template instantiation.
