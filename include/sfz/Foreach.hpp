e0db314fe01817abe8a43a5dcae36149d55035bc
Chris Pickel <sfiera@gmail.com>
2010-07-02 00:24:50 -0400

    Simple try at getting foreach() in MSVC 10.
    I wish waiting for 'for (x : y)' was a better option.

--- include/sfz/Foreach.hpp
+++ include/sfz/Foreach.hpp
@@ -6,10 +6,19 @@
 #ifndef SFZ_FOREACH_HPP_
 #define SFZ_FOREACH_HPP_
 
+#   if (_MSC_VER >= 1600) && (_MSC_VER < 1700)
+#       define SFZ_DECLTYPE(x) decltype(x)
+#   elif __GNUC__ == 4
+#       define SFZ_DECLTYPE(x) __typeof__(x)
+#   else
+#       define SFZ_DECLTYPE(x) int
+#       error "Don't know how to get decltype-like functionality"
+#   endif
+
 #define foreach(VAR, CONTAINER) \
     for (bool __loop = true; __loop; ) \
-    for (const typeof(CONTAINER)& __container = (CONTAINER); __loop; __loop = false) \
-    for (typeof(__container.begin()) VAR = __container.begin(), __end = __container.end(); \
+    for (const SFZ_DECLTYPE(CONTAINER)& __container = (CONTAINER); __loop; __loop = false) \
+    for (SFZ_DECLTYPE(__container.begin()) VAR = __container.begin(), __end = __container.end(); \
             VAR != __end; ++VAR)
 
 #endif  // SFZ_FOREACH_HPP_
